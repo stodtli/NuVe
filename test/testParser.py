0ef913b3c865aba802f6268e886a3593aa5f471f
xifangbubai <xifangbubai>
2017-03-04 14:23:48 -0800

    More test cases for Parser

--- test/testParser.py
+++ test/testParser.py
@@ -10,7 +10,7 @@ from Parser import Parser
 
 class TestParser(unittest.TestCase):
 
-    # Ensure that ApproxJacobian returns the expected value on a 1D input
+    # Ensure that the config file is read in properly
     def testReadConfigFile(self):
 
         # Make sure Parser throws an exception if it's given a bad config filename
@@ -24,6 +24,25 @@ class TestParser(unittest.TestCase):
             raise Exception('Parser failed to raise an exception on a nonstring input')
         except TypeError:
             None
+
+        # try on some bad input files
+        try:
+            p = Parser("example_bad_config_01.ini")
+            raise Exception('Parser failed to raise an exception on a bad configuration file')
+        except IOError:
+            None
+        try:
+            p = Parser("example_bad_config_02.ini")
+            raise Exception('Parser failed to raise an exception on a bad configuration file')
+        except IOError:
+            None
+
+
+        # try on a good file
+        p = Parser("example_config_01.ini")
+        print(p._parameters)
+        assert( p._field_names ==  {"mu":"MU","x":"X1","t":"T"})
+        #assert( cmp( set(p._spatial_coords), set(["x"]) ) == 0)
 
 
 if __name__ == '__main__':
