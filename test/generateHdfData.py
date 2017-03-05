7ce7f1fef0623b69a29e5894098c9c7cbda50afe
Simon Todtli <stoedtli@hotmail.com>
2017-03-04 16:08:34 -0800

    Extended example read and write functions to multiple dimensions

--- test/generateHdfData.py
+++ test/generateHdfData.py
@@ -3,24 +3,27 @@ import numpy as np
 
 # spatial coordinate vector (assumed to not change with time)
 x = np.linspace(0.0, 1.0, num = 10, endpoint = True)
+y = np.linspace(0.0, 1.0, num = 5, endpoint = True)
 
 # data first snapshot
 t1 = 0.0
-u1 = np.arange(0, x.size)
+u1 = np.arange(0, x.size*y.size,).reshape(x.size, y.size, order = 'F')
 
 # data second snapshot
 t2 = 0.02
-u2 = np.arange(x.size, 2 * x.size)
+u2 = np.arange(x.size*y.size, 2*x.size*y.size).reshape(x.size, y.size, order = 'F')
 
 # write snapshots to hdf5 file
 f1 = h5py.File('testrun_001.hdf5', 'w')
-f1.create_dataset("x", data = x)
+f1.create_dataset("X1", data = x)
+f1.create_dataset("X2", data = y)
 f1.create_dataset("t", data = t1)
 f1.create_dataset("u", data = u1)
 f1.close()
 
 f2 = h5py.File('testrun_002.hdf5', 'w')
-f2.create_dataset("x", data = x)
+f2.create_dataset("X1", data = x)
+f2.create_dataset("X2", data = y)
 f2.create_dataset("t", data = t1)
 f2.create_dataset("u", data = u1)
 f2.close()
--- test/readHdfData.py
+++ test/readHdfData.py
@@ -1,23 +1,22 @@
 import numpy as np
 import h5py
 
-spatial_coords = ["x"]
+spatial_coords = ["x", "y"]
 temporal_coord = "t"
 field_names = {
-        "x": "x",
+        "x": "X1",
+        "y": "X2",
         "u": "u",
         "t": "t",
         }
 
 # retreive data from hdf5 file
-# data = dict((x, []) for x in field_names.keys())
 data = {}
 
 with h5py.File("testrun_001.hdf5", "r") as f:
     for field in field_names.keys():
-        data[field] = np.array(f[field])
-    # data["u"]=np.array(f["u"])
+        data[field] = np.array(f[field_names[field]]).flatten('F')
 
-print(data["t"])
+print(data["u"])
 
 
