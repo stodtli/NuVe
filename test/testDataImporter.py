054f2ef8838c647692a17f59a35fb3d848ecd733
Simon Todtli <stoedtli@hotmail.com>
2017-03-04 19:44:59 -0800

    Implementation of testDataImporter cleaned

--- test/testDataImporter.py
+++ test/testDataImporter.py
@@ -169,33 +169,7 @@ class TestDataImporter(unittest.TestCase):
 
 
 
-
-        
-
 if __name__ == '__main__':
     unittest.main()
-
-
-        
-
-# try read data
-# run_name = "testrun"
-# start_nr = 1
-# temp_deriv_order = 1
-# nr_digits = 3
-# spatial_coords = ["x", "y"]
-# temporal_coord = "t"
-# field_names = {
-        # "x": "X1",
-        # "y": "X2",
-        # "u": "u",
-        # "t": "t",
-        # }
-
-# importer = ColumnMajorImporter()
-# data = importer.importData(run_name, start_nr, temp_deriv_order, nr_digits,
-        # field_names, spatial_coords, temporal_coord)
-
-# print(data["u"])
 
 
